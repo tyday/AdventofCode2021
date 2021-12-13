61742a03d4f33406213e383795dc28c199f1d3a0
Tyler Day <tyrday@gmail.com>
2021-12-12 21:25:11 -0500

    Day 12 Part 2 not working

--- Day12/app.py
+++ Day12/app.py
@@ -2,55 +2,86 @@
 #
 # Created: 12-12-2021 07:41:24
 # Creator: Ty Day
+
+import string
+
+class Vertex:
+    def __init__(self, val) -> None:
+        self.val = val
+        self.revisitable = False if val[0] in string.ascii_lowercase else True
+        self.neighbors = set()
+
 class Graph:
     def __init__(self, data):
-        self.Vertices = set()
-        self.Edges = set()
-        self.Neighbors = {}
-        for edge in data:
-            a,b = edge.split('-')
-            self.Vertices.add(a)
-            self.Vertices.add(b)
-            self.Edges.add(frozenset({a,b}))
-            if a in self.Neighbors:
-                self.Neighbors[a].append(b)
-            else:
-                self.Neighbors[a] = [b]
-            if b in self.Neighbors:
-                self.Neighbors[b].append(a)
-            else:
-                self.Neighbors[b] = [a]
-class Queueing_structure:
-    def __init__(self, type):
-        self.type = type
-        self.data = []
-    def __len__(self):
-        return(len(self.data))
-    def push(self, item):
-        self.data.append(item)
-    def pop(self):
-        if self.type.lower() == 'dfs':  # Stack
-            return self.data.pop()
-        elif self.type.lower() == 'bfs':    #Queue
-            return self.data.pop(0)
-        else:
-            raise Exception("Unknown queueing structure")
-def traversal(start_vertex, graph, structure):
-    queueing_structure = Queueing_structure(structure)
-    queueing_structure.push((start_vertex, None))
-    explored_vertices = []
-    routing_table = {}
-    while len(queueing_structure) > 0:
-        current_vertex, parent = queueing_structure.pop()
-        if not current_vertex in explored_vertices:
-            explored_vertices.append(current_vertex)
-            routing_table[current_vertex] = parent
-            for neighbor in graph.Neighbors[current_vertex]: # TODO implement this
-                if neighbor not in explored_vertices:
-                    queueing_structure.push((neighbor, current_vertex))
-    return explored_vertices, routing_table
-    
+        self.graph = {}
+        for item in data:
+            self.add_vertice(item)
+        # dict(sorted(d.items()))
+        self.graph =dict(sorted(self.graph.items()))
+    def add_vertice(self, item):
+        a,b = item.split('-')
+        a_not_revisitable = True if a[0] in string.ascii_lowercase else False
+        b_not_revisitable = True if b[0] in string.ascii_lowercase else False
+        if a in self.graph:
+            self.graph[a]['neighbors'].add(b)
+        else: self.graph[a] = {
+            'neighbors':{b},
+            'not_revisitable': a_not_revisitable
+            }
+        if b in self.graph:
+            self.graph[b]['neighbors'].add(a)
+        else: self.graph[b] = {
+            'neighbors':{a},
+            'not_revisitable': b_not_revisitable
+            }
 
+def find_paths(graph, start_vertex,visited):
+    if start_vertex == 'end':
+        return 1
+    elif len(graph[start_vertex]['neighbors']) < 1:
+        return 0
+    else:
+        local_visited = visited[:]
+        if  graph[start_vertex]['not_revisitable']:
+            local_visited += [start_vertex]
+        neighbors = graph[start_vertex]['neighbors']
+        neighbors = [n for n in neighbors if n not in visited]
+        results = 0
+        for neighbor in neighbors:
+            results += find_paths(graph,neighbor,local_visited)
+        return results
+
+def find_paths_2(graph, start_vertex, visited, visited_once):
+    neighbors = graph[start_vertex]['neighbors']
+    neighbors = [n for n in neighbors if n not in visited]
+    if start_vertex == 'end':
+        return [['end']]
+    elif len(neighbors) < 1:
+        return [[start_vertex]]
+    else:
+        local_visited = visited[:]
+        local_visited_once = visited_once[:]
+        if  graph[start_vertex]['not_revisitable']:
+            if start_vertex == 'start' or start_vertex == 'end':
+                local_visited += [start_vertex]
+            elif start_vertex in local_visited_once:
+                local_visited += [start_vertex]
+            else:
+                local_visited_once.append(start_vertex)
+
+        # neighbors = graph[start_vertex]['neighbors']
+        # neighbors = [n for n in neighbors if n not in visited]
+        routes = []
+        for neighbor in neighbors: #start,A,b,A,c,A,c,A,end
+            result = find_paths_2(graph,neighbor,local_visited,local_visited_once)
+            for r in result:
+                routes.append(r)
+        [r.insert(0, start_vertex) for r in routes]
+        return routes
+
+def test(a):
+    a = 5
+    print(a)
 
 if __name__ == '__main__':
     data = ''
@@ -60,8 +91,13 @@ if __name__ == '__main__':
     
     print(data)
     graph = Graph(data)
-    ev,rt = traversal('start',graph,'bfs')
-    print(ev)
-    # print(rt)
-    for k,v in rt.items():
-        print(k,v)
+    for vertix in graph.graph:
+        graph.graph[vertix]['neighbors']= sorted(graph.graph[vertix]['neighbors'])
+    # print(graph.graph)
+    # print(find_paths(graph.graph, 'start',[]))
+    routes = find_paths_2(graph.graph, 'start',[], [])
+    # r = [print(",".join(r)) for r in routes if r[-1] == 'end']
+    r = [print(",".join(r)) for r in routes if r[-1] == 'end']
+    print(len(r))
+
+    
